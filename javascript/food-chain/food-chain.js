fe52f4ff9faaafe942b3a0eef62390e01764f3c1
laseq <lourenco.sequeira@outlook.com>
2017-06-20 00:03:58 +0100

    Done the code for the animals one at a time

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -4,19 +4,63 @@ function FoodChain() {
 
 function verse(num) {
   let songLyrics = '';
-  switch (num) {
-    case 1:
-      songLyrics = `${lyrics.base} fly.\n${lyrics.fly}`;
-      break;
-    default:
-      songLyrics = 'Something went wrong';
+  const lyricsObj = new Lyrics(creatureMap[num]);
+  // switch (num) {
+  //   case 1:
+  //     songLyrics = `${lyricsObj.base}${lyricsObj.fly}`;
+  //     break;
+  //   case 2:
+  //     songLyrics = `${lyricsObj.base}${specialLyrics.spider}${lyricsObj.spider}`;
+  //     break;
+  //   default:
+  //     songLyrics = 'Something went wrong';
+  // }
+  if (typeof specialLyrics[creatureMap[num]] === 'string') {
+    songLyrics = `${lyricsObj.base}${specialLyrics[creatureMap[num]]}${lyricsObj[creatureMap[num]]}`;
+  } else {
+    songLyrics = `${lyricsObj.base}${lyricsObj[creatureMap[num]]}`;
   }
+
   return songLyrics;
 }
 
-const lyrics = {
-  base: 'I know an old lady who swallowed a',
-  fly: 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n'
+// const lyrics = {
+//   base: 'I know an old lady who swallowed a',
+//   fly: 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n',
+//   spider: `She swallowed the spider to catch the fly.\n${fly}`
+// };
+
+function Lyrics(creature) {
+  this.creature = creature;
+  this.base = `I know an old lady who swallowed a ${this.creature}.\n`;
+  this.fly = 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
+  this.spider = `She swallowed the spider to catch the fly.\n${this.fly}`;
+  this.bird = `She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n${this.spider}`;
+  this.cat = `She swallowed the cat to catch the bird.\n${this.bird}`;
+  this.dog = `She swallowed the dog to catch the cat.\n${this.cat}`;
+  this.goat = `She swallowed the goat to catch the dog.\n${this.dog}`;
+  this.cow = `She swallowed the cow to catch the goat.\n${this.goat}`;
+  this.horse = `She\'s dead, of course!\n`;
+}
+
+const specialLyrics = {
+  spider: 'It wriggled and jiggled and tickled inside her.\n',
+  bird: 'How absurd to swallow a bird!\n',
+  cat: 'Imagine that, to swallow a cat!\n',
+  dog: 'What a hog, to swallow a dog!\n',
+  goat: 'Just opened her throat and swallowed a goat!\n',
+  cow: 'I don\'t know how she swallowed a cow!\n'
+};
+
+const creatureMap = {
+  1: 'fly',
+  2: 'spider',
+  3: 'bird',
+  4: 'cat',
+  5: 'dog',
+  6: 'goat',
+  7: 'cow',
+  8: 'horse'
 };
 
 module.exports = FoodChain;
