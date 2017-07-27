8595aaa7eafaee334c848a1131e2f863f5f0b6c8
laseq <lourenco.sequeira@outlook.com>
2017-07-27 15:53:08 +0100

    Completed exercise

--- javascript/sublist/sublist.js
+++ javascript/sublist/sublist.js
@@ -7,29 +7,10 @@ function compareList(listToCompare) {
   let listOne = [];
   let listTwo = [];
 
-  // if (this.list) listOne = numberSort(this.list);
-  // if (listToCompare.list) listTwo = numberSort(listToCompare.list);
   if (this.list) listOne = this.list;
   if (listToCompare.list) listTwo = listToCompare.list;
 
-  const result = (checkEquality(listOne, listTwo) || checkSublist(listOne, listTwo) || checkSuperlist(listOne, listTwo));
-  return result;
-
-  // if (checkEquality(listOne, listTwo) === 'EQUAL'){
-  //   return 'EQUAL';
-  // } else if (checkEquality(listOne, listTwo) === 'UNEQUAL') {
-  //   return 'UNEQUAL';
-  // }
-  // if (checkSublist(listOne, listTwo) === 'SUBLIST') {
-  //   return 'SUBLIST';
-  // } else if (checkSublist(listOne, listTwo) === 'UNEQUAL') {
-  //   return 'UNEQUAL';
-  // }
-  // if (checkSuperlist(listOne, listTwo) === 'SUPERLIST') {
-  //   return 'SUPERLIST';
-  // } else if (checkSuperlist(listOne, listTwo) === 'UNEQUAL') {
-  //   return 'UNEQUAL';
-  // }
+  return (checkEquality(listOne, listTwo) || checkSublist(listOne, listTwo) || checkSuperlist(listOne, listTwo));
 }
 
 function checkEquality(listOne, listTwo) {
@@ -51,18 +32,6 @@ function checkSublist(listOne, listTwo) {
   }
 }
 
-function checkElementsExist(array1, array2) {
-  // .every returns true if every element passes the test
-  // return array1.every(value => {
-  //   const index = array2.indexOf(value);
-  //   return (index === 0) ? true : index;
-  // });
-
-  const string1 = array1.join(',');
-  const string2 = array2.join(',');
-  return string2.includes(string1);
-}
-
 function checkSuperlist(listOne, listTwo) {
   if (listOne.length && !listTwo.length) return 'SUPERLIST';
   if ((listOne.length > listTwo.length) && checkElementsExist(listTwo, listOne))  {
@@ -72,10 +41,10 @@ function checkSuperlist(listOne, listTwo) {
   }
 }
 
-function numberSort(array) {
-  return array.sort((a, b) => {
-    return a-b;
-  });
+function checkElementsExist(array1, array2) {
+  const string1 = array1.join(',');
+  const string2 = array2.join(',');
+  return string2.includes(string1);
 }
 
 module.exports = List;
